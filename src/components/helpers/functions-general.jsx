921d603f41e4bd8e691046cf4ab44d1de376f11a
CyreneLumabas <cyrene.lumabas@frontlinebusiness.com.ph>
2024-09-18 15:33:27 +0800

    update

--- src/components/helpers/functions-general.jsx
+++ src/components/helpers/functions-general.jsx
@@ -11,6 +11,7 @@ export const devNavUrl = "";
 export const ver = "v2";
 export const urlSystem = "developer";
 
+// test
 export const devKey =
   "$2a$12$47wDvbLInZif/PVS8B6P3.7WxyJvUpBzZAWCsnWJUKq3nrn4qgmeO";
 
