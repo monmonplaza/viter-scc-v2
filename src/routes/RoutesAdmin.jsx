6eb388443c787a93a1d43efd0566b9bf0508a8f7
monmonplaza <monmon.plaza@gmail.com>
2024-09-23 15:38:55 +0800

    123

--- src/routes/RoutesAdmin.jsx
+++ src/routes/RoutesAdmin.jsx
@@ -3,12 +3,97 @@ import OtherProtectedRoute from "@/components/pages/developer/access/OtherProtec
 import Overview from "@/components/pages/developer/overview/Overview.jsx";
 import Settings from "@/components/pages/developer/settings/Settings.jsx";
 
-export const RoutesAdmin = [
+export const routesAdmin = [
   {
     path: `${devNavUrl}/${urlAdmin}/overview`,
     element: (
       <OtherProtectedRoute>
         <Overview />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/inventory`,
+    element: (
+      <OtherProtectedRoute>
+        <Inventory />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/receiving`,
+    element: (
+      <OtherProtectedRoute>
+        <Receiving />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/defective-product`,
+    element: (
+      <OtherProtectedRoute>
+        <DefectiveProduct />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/sales`,
+    element: (
+      <OtherProtectedRoute>
+        <Sales />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/products`,
+    element: (
+      <OtherProtectedRoute>
+        <Product />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/product-price`,
+    element: (
+      <OtherProtectedRoute>
+        {" "}
+        <ProductPrice />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/suppliers`,
+    element: (
+      <OtherProtectedRoute>
+        {" "}
+        <Supplier />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/customers`,
+    element: (
+      <OtherProtectedRoute>
+        {" "}
+        <Customers />
+      </OtherProtectedRoute>
+    ),
+  },
+  {
+    path: `${devNavUrl}/${urlAdmin}/return-product`,
+    element: (
+      <OtherProtectedRoute>
+        <ReturnProduct />
+      </OtherProtectedRoute>
+    ),
+  },
+
+  {
+    path: `${devNavUrl}/${urlAdmin}/unit`,
+    element: (
+      <OtherProtectedRoute>
+        {" "}
+        <Unit />
       </OtherProtectedRoute>
     ),
   },
@@ -18,6 +103,15 @@ export const RoutesAdmin = [
     element: (
       <OtherProtectedRoute>
         <Settings />
+      </OtherProtectedRoute>
+    ),
+  },
+
+  {
+    path: `${devNavUrl}/${urlAdmin}/purchase`,
+    element: (
+      <OtherProtectedRoute>
+        <Purchase />
       </OtherProtectedRoute>
     ),
   },
